1fd31ea5c55632542d6c32994cc8e26b73f94ae3
ocean0x42 <81422224+ocean0x42@users.noreply.github.com>
2022-01-24 15:22:50 +0800

    删除cpp1文件
    第一次使用GitHub删除文件，练习一把

--- cpp1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "hello, world!" << endl;
-    cout << "I get a change in cpp1 file.That looks cool!right?" << endl;
-    return 0;
-}
